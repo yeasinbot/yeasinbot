9a51a8d31f9541f264a988e820924732165bfb82
Yaseen270 <bdgamesbazar71@gmail.com>
2024-12-22 21:52:13 +0600

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002443093710')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+kNCPQw8j1Aw5MWY1")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/+kNCPQw8j1Aw5MWY1")
